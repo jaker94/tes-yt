6d902db748d0d4cc75314591791260318be920f2
Jacob <jakeregister94@gmail.com>
2023-04-10 19:48:48 -0600

    headerBlock in progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './Components/Menu/Menu';
+import HeaderBlock from './Components/Header/HeaderBlock';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
       {isMenuOpen && <Menu/>}
+      <HeaderBlock/>
     </div>
     </Router>
   );
